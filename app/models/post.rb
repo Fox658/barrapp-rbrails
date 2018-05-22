dd7071a5ea2f2af6b076791b83dbb0451e251d2b
Fox658 <j.c.alvarez658@gmail.com>
2018-05-21 20:28:19 -0400

    Add Bootstrap, css and banner urls and date

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
+  scope :most_recent, -> {order(id: :desc)}
   def should_generate_new_friendly_id?
     title_changed?
+  end
+  def display_day_published
+    "Publicado el #{created_at.strftime('%-d/%m/%Y')}"
   end
 
 end
