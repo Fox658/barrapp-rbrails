3787789b27922ca8fbde198a990e93922c779751
Fox658 <j.c.alvarez658@gmail.com>
2018-05-22 20:39:07 -0400

    add author CRUD pages reorganize files to reporpuse for author.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
-  resources :posts
-  #root to: 'rooms#show'
   get 'rooms/show'
-  get 'about' => 'pages#about', as: :about
-  get 'contact' => 'pages#contact', as: :contact
+
+  root to: 'blog/posts#index'
+  namespace :author do
+    resources :posts
+  end
+
+  scope module: 'blog' do
+    get 'about' => 'pages#about', as: :about
+    get 'contact' => 'pages#contact', as: :contact
+    get 'posts' => 'posts#index', as: :posts
+    get 'posts/:id' => 'posts#show', as: :post
+  end
+
+  #root to: 'rooms#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
