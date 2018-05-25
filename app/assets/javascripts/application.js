bf15911c8e6d9db5304229b4fecc613af09619a5
Fox658 <j.c.alvarez658@gmail.com>
2018-05-25 00:13:10 -0400

    add tinymce and customs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,13 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("turbolinks:load", function() {
+  tinymce.remove();
+  tinymce.init({
+    selector:'textarea#post_body',
+    plugins: "codesample image media link",
+
+    toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | codesample | image | media | link"
+  });
+})
