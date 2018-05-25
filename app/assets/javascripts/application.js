35e7a330405554d83f77f1df80bf91704e6ab2e6
Fox658 <j.c.alvarez658@gmail.com>
2018-05-25 18:33:25 -0400

    Add scss styles and astetics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,9 +22,9 @@
 document.addEventListener("turbolinks:load", function() {
   tinymce.remove();
   tinymce.init({
+    height: '475',
     selector:'textarea#post_body',
     plugins: "codesample image media link",
-
     toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | codesample | image | media | link"
   });
 })
